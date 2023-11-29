89dc770ead67bae3858b090decf8fed847282a78
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2023-11-29 22:23:06 +0100

    page projets

---
+++ pages/projets/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="general.pageBg">
+    <NuxtLayout 
+    name="custom"
+    title="Projets"
+    :page-name="$route.name">
+
+    <div class="page-content">
+      <nuxt-link
+        v-for="_, i in 10"
+        :key="i"
+        class="page-content__photo"
+        :to="`/projets/${i+1}`">
+          <img src="/photo.jpeg" alt="">
+      </nuxt-link>
+    </div>
+  </NuxtLayout>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .page-content {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 10px;
+    align-items: flex-start;
+  }
+</style>
