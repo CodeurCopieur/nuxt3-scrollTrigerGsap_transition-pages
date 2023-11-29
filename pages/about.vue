172b2737567672630a3663843d54e5323dedcbd6
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2023-11-29 22:00:57 +0100

    add content

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <h1>About</h1>
+  <div :class="general.pageBg">
+    <NuxtLayout 
+    name="custom"
+    title="About"
+    :page-name="$route.name">
+    <div class="page_content">
+      <div 
+        v-for="_ in 10"
+        :key="_"
+        class="page-content__block p1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nostrum sit cum omnis assumenda? Placeat vitae similique assumenda, deleniti nisi voluptates cum consequatur labore dolore et repudiandae deserunt modi molestias voluptas aliquam quas quidem. Mollitia, accusamus? Nesciunt distinctio minus pariatur.
+      </div>
+    </div>
+  </NuxtLayout>
   </div>
- </template>
+</template>
+<style lang="scss" scoped>
+  .page-content__block {
+    padding-bottom: 32px;
+  }
+</style>
