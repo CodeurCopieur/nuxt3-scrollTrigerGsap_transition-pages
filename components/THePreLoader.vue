dcf5ebdcffae6de15b4cb1edac1d9a1944e4c5e3
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2023-11-30 11:18:00 +0100

    New Composant ThePreLoader

---
+++ components/THePreLoader.vue
@@ -0,0 +1,23 @@
+<script setup>
+  const percentage = ref(0);
+</script>
+
+<template>
+  <div class="preloader">
+    <div class="preloader-precent h1">
+      {{ percentage }}%
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .preloader {
+    position: fixed;
+    inset: 0;
+    background-color: $white;
+    z-index: $z-preloader;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="viewport">
+    <THePreLoader/>
     <slot />
   </div>
 </template>
