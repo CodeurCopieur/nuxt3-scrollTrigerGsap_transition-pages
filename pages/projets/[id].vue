7acb1cedfd8d4e22b9afa0c061621af2e62cffb2
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2023-11-30 01:58:23 +0100

    page projet

---
+++ pages/projets/[id].vue
@@ -0,0 +1,49 @@
+<template>
+  <div :class="general.pageBg">
+    <NuxtLayout 
+    name="custom"
+    :title="`Projet ${$route.params.id}`"
+    :page-name="`projet-${$route.params.id}`">
+
+    <div class="page-content">
+      <div 
+        v-for="n in 3"
+        :key="n"
+        class="page-content__block">
+        
+        <div class="page-content__block-photo">
+          <img src="/photo.jpeg" alt="">
+        </div>
+
+        <div class="page-content__block-text p1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quos repudiandae rem! Sed expedita eius libero, a architecto hic officia.
+        </div>
+        
+
+      </div>
+
+      <div class="page-next__project">
+          <nuxt-link
+            class="page-next__project-link hover-fadein h2"
+            :to="`/projets/${+$route.params.id+1}`">
+            Projet suivant - {{ +$route.params.id+1 }}
+          </nuxt-link>
+        </div>
+    </div>
+  </NuxtLayout>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .page-content__block {
+    padding: 32px 0;
+  }
+
+  .page-content__block-text {
+    margin-top: 16px;
+  }
+
+  .page-next__project {
+    padding-bottom: 32px;
+    margin-top: 32px;
+  }
+</style>
