e268667e3641506e5880a67082eab9aa75122533
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2023-12-06 00:35:55 +0100

    new property

--- store/index.js
+++ store/index.js
@@ -3,5 +3,7 @@ import { reactive } from 'vue'
 export const general = reactive({
   pageBg: 'page_black',
   isPreloaderVisible: true,
-  scrollLenis: null
+  scrollLenis: null,
+  isTransitionStart: true,
+  isTransitionFinish: true
 })
