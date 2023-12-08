7a08b41ffa95e1c5734b51ec3f8695a064195a3d
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2023-12-08 15:49:21 +0100

    Update firstScreeenAnimation.js

--- animations/firstScreeenAnimation.js
+++ animations/firstScreeenAnimation.js
@@ -3,8 +3,13 @@ export function firstScreenAnimation({parent}) {
   const parentEl = document.querySelector(parent)
   const header = parentEl.querySelector('.header')
   const title = parentEl.querySelector('.default-title__wrap')
+  const mask = parentEl.querySelector('.page-mask')
 
   const tl = gsap.timeline()
+
+  tl.to(mask, {
+    opacity: 0
+  })
 
   tl.from(title, {
     yPercent: 100,
