fc6e85b3791efe551dfec634b3792bf69b06e7ea
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2023-12-08 15:45:42 +0100

    Create PageMask.vue

---
+++ components/PageMask.vue
@@ -0,0 +1,25 @@
+<script setup>
+  
+</script>
+
+<template>
+  <div class="page-mask">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .page-mask {
+    position: fixed;
+    inset: 0;
+    z-index: $z-mask;
+    pointer-events: none;
+  }
+
+  .page_white .page-mask {
+    background: $white;
+  }
+  .page_black .page-mask {
+    background: $black;
+  }
+</style>
